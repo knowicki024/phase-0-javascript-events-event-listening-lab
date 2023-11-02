c44642a2e4e1444da3ae1696fbfb025fa47d9daf
Kaitlyn Nowicki <katie.nowicki024@gmail.com>
2023-11-02 11:15:21 -0400

    done

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-function addingEventListener() {
-}
+function clickAlert() {
+    alert('I was clicked!');
+  }
+  
+  function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', clickAlert);
+  }
